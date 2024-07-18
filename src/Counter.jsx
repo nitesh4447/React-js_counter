1317a55ef534c971b54238f2afaadaeeb626bbc7
nitesh patharkar <143060767+nitesh4447@users.noreply.github.com>
2024-07-18 15:53:26 +0530

    Update Counter.jsx

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -1,17 +1,16 @@
 import React, { useState } from "react"
-
 function Counter(){
  const [Counter,setCounter]=useState(0)
     return(
-        <div>
+        <div className="count">
         
-            <h1>Counter : {Counter}</h1>
+            <h1>COUNTER : {Counter}</h1>
 
             <button disabled={Counter==0} onClick={() => setCounter(Counter - 1)}>-</button>
-            <button onClick={() => setCounter(0)}>reset</button>
+            <button onClick={() => setCounter(0)}>RESET</button>
             <button onClick={() => setCounter(Counter + 1)}>+</button>
             
-            <h1>{Counter %2==0?"odd":"even"}</h1> 
+            <h1> THIS NUMBER IS :{Counter %2==0?"EVEN":"ODD"}</h1> 
       
         </div>
     )
